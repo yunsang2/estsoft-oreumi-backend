2d971109b82330711863bfcd654e085bc17f7032
Sangee Yoon <sk41442457@gmail.com>
2025-11-28 16:14:53 +0900

    java 과제

---
+++ 14-java/src/Day1/Day1_3.java
@@ -0,0 +1,16 @@
+package Day1;
+
+import java.io.PrintStream;
+
+public class Day1_3 {
+    public static void main(String[] args) {
+        int x = 10, y = 3;
+
+        System.out.printf("x = %d, y = %d\n", x, y);
+        System.out.printf("x + y = %d\n", x + y);
+        System.out.printf("x - y = %d\n", x - y);
+        System.out.printf("x * y = %d\n", x * y);
+        System.out.printf("x / y = %.2f\n", (double)x / y);
+        System.out.printf("x %% y = %d\n", x % y);
+    }
+}
