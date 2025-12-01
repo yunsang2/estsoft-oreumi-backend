b1b64448747c551c71960fed676f449c6a770b6b
Sangee Yoon <sk41442457@gmail.com>
2025-12-01 17:45:01 +0900

    Create Day2_Ex1.java

---
+++ 14-java/src/Day2/Day2_Ex1.java
@@ -0,0 +1,27 @@
+package Day2;
+import java.util.Scanner;
+public class Day2_Ex1 {
+    public static void main(String[] args) {
+
+        int hourlyWage;
+        int hours;
+        int todayWage;
+
+        Scanner scan = new Scanner(System.in);
+
+        hourlyWage = scan.nextInt();
+
+        System.out.print("Enter the hourly Wage: ");
+        hours = scan.nextInt();
+        System.out.println();
+
+        scan.close();
+
+        if (hours > 8)
+           todayWage = hourlyWage * 8 + (int)(hourlyWage * 1.5 * (hours - 8));
+        else
+           todayWage = hourlyWage * hours;
+
+        System.out.printf("Today's wage is %d won.\n", todayWage);
+    }
+}
