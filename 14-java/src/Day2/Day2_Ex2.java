fec88896768b13a823995717a693925fc6fcceea
Sangee Yoon <sk41442457@gmail.com>
2025-12-01 17:45:07 +0900

    Create Day2_Ex2.java

---
+++ 14-java/src/Day2/Day2_Ex2.java
@@ -0,0 +1,19 @@
+package Day2;
+import java.util.Scanner;
+public class Day2_Ex2 {
+    public static void main(String[] args) {
+
+        int num;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a positive integer: ");
+        num = scan.nextInt();
+        scan.close();
+
+
+
+        for (int i = 1; i <= num; i *= 10) {
+            System.out.printf("%d's digit = %d\n", i, num / i % 10);
+        }
+    }
+}
