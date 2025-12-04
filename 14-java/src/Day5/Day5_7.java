986eccd00dbcaf25e2d570b36188cb7ad67a3898
Sangee Yoon <sk41442457@gmail.com>
2025-12-04 16:10:42 +0900

    Studsent grades

---
+++ 14-java/src/Day5/Day5_7.java
@@ -0,0 +1,16 @@
+package Day5;
+
+public class Day5_7 {
+    public static void main(String[] args) {
+
+    Student kim = new Student("Cheolsu Kim", 88, 76, 64);
+    Student pack = new Student("Yeonghee Pack", 92, 88, 94);
+    Student hong = new Student("Gildong Hong", 96, 64, 48);
+
+
+    kim.print();
+    pack.print();
+    hong.print();
+
+    }
+}
